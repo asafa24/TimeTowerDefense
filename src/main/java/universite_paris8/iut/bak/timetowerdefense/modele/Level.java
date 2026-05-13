33fa8008bfcab7b5e64f53ac568cb215b2f28a6a
Bachir TAHENNI <btahenni@iut.univ-paris8.fr>
2026-05-13 15:53:25 +0200

    suppression du s dans titre de la scene

--- src/main/java/universite_paris8/iut/bak/timetowerdefense/modele/Level.java
+++ src/main/java/universite_paris8/iut/bak/timetowerdefense/modele/Level.java
@@ -5,12 +5,27 @@ public class Level {
 
     public Level() {
         tileMap = new int[5][19][25];
+        premierChemin(tileMap);
+    }
+    public void premierChemin (int [][][] map) {
+        int monde = 0;
+        int hauteur = 17;
+        int largeur = 0 ;
+        // ligne 1
+        for (int i = 0; i < 5; i++) {
+            largeur = i;
+            map[monde][hauteur][largeur] = 3;
+
+        }
+        map[monde][hauteur][5] = 4;
+
 
     }
 
     public int[][] loadLevel(int epoque) {
         return tileMap[epoque];
     }
-
-
 }
+
+
+
