923ed4f6fc4c2a00a926372a41cbf61a87d6a5a7
kev1jen1 <100167591+kev1jen1@users.noreply.github.com>
2026-05-19 10:19:02 +0200

    ajout de la classe Vague avec des levels sous forme de tabledau de int ou {5,3,2,1} signifie 5 ennemies basiques, 3 speciales , 2 tanks , 1 bosse

---
+++ src/main/java/universite_paris8/iut/bak/timetowerdefense/modele/Vague.java
@@ -0,0 +1,21 @@
+package universite_paris8.iut.bak.timetowerdefense.modele;
+
+public class Vague {
+    public int[][] level1 ;
+
+    public Vague(){
+        level1 = new int[][]{
+                {5,0,0,0}, // vague1
+                {10,0,0,0}, // vague 2
+                {20,2,0,0}, // vague 3
+                {30,3,1,0}, // vague 4
+                {40,4,2,0}, // vague 5
+                {45,5,3,0}, // vague 6
+                {50,6,3,0}, // vague 7
+                {55,7,4,0}, // vague 8
+                {60,10,6,0}, // vague 9
+                {75,12,8,1} // vague 10 avec le boss ( nn c'est toi tié le boss )
+
+        };
+    }
+}
