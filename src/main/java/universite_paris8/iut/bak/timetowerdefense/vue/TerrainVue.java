689600fb4f9bc497b1d874a3bfaa4d51e429cdb5
Kevin JENY <kjeny@iut.univ-paris8.fr>
2026-05-21 10:00:36 +0200

    (modif) refonte du terrain 200% moche

--- src/main/java/universite_paris8/iut/bak/timetowerdefense/vue/TerrainVue.java
+++ src/main/java/universite_paris8/iut/bak/timetowerdefense/vue/TerrainVue.java
@@ -8,7 +8,8 @@ import javafx.scene.shape.Rectangle;
 import universite_paris8.iut.bak.timetowerdefense.Application;
 
 public class TerrainVue {
-
+    private final int  POURCENTAGE_FLEUR = 50;
+    private final int  POURCENTAGE_PROPS = 20;
     private Pane backgroundPane;
 
     private static final int TILE_SIZE = 64;
@@ -34,6 +35,14 @@ public class TerrainVue {
                 if (tileId == 7){
                     afficherTile("",7,x,y);
                 }
+
+                else if (tileId == 0 && randomInRange(0,100)< POURCENTAGE_PROPS){
+                    {
+                        afficherProps("prehistoire/", x, y);
+
+                    }
+                }
+
                 else if (tileId != 0){
                     afficherTile("prehistoire/",tileId, x , y);
                 }
@@ -53,13 +62,29 @@ public class TerrainVue {
         backgroundPane.getChildren().add(machine);
     }
     public void afficherSol(String epoque, int x,int y){
-        ImageView machine =  new ImageView(String.valueOf(Application.class.getResource("images/tiles/"+epoque+"0"+randomInRange(1,5)+".png" )));
+        ImageView machine;
+        if (randomInRange(0,100) < POURCENTAGE_FLEUR){
+             machine =  new ImageView(String.valueOf(Application.class.getResource("images/tiles/"+epoque+"0"+randomInRange(1,5)+".png" )));
+        }
+        else {
+            machine =  new ImageView(String.valueOf(Application.class.getResource("images/tiles/"+epoque+"000"+randomInRange(1,2)+".png" )));
+        }
+
         machine.setFitWidth(TILE_SIZE);
         machine.setFitHeight(TILE_SIZE);
         machine.setX(x * TILE_SIZE);
         machine.setY(y * TILE_SIZE);
 
         machine.setRotate(rotationAlea(4));
+        backgroundPane.getChildren().add(machine);
+    }
+    public void afficherProps(String epoque,int x, int y){
+        ImageView machine =  new ImageView(String.valueOf(Application.class.getResource("images/tiles/"+epoque+"00"+randomInRange(1,5)+".png" )));
+        machine.setFitWidth(TILE_SIZE);
+        machine.setFitHeight(TILE_SIZE);
+        machine.setX(x * TILE_SIZE);
+        machine.setY(y * TILE_SIZE);
+
         backgroundPane.getChildren().add(machine);
     }
 
