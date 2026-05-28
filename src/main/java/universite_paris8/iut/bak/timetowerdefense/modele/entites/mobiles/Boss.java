b54b57bcf1f6538f1941172117b72fd136cb4e1a
Asafa Saidi <asaidi@iut.univ-paris8.fr>
2026-05-28 11:53:00 +0200

    Ajout compétence boss (pas fini)

--- src/main/java/universite_paris8/iut/bak/timetowerdefense/modele/entites/mobiles/Boss.java
+++ src/main/java/universite_paris8/iut/bak/timetowerdefense/modele/entites/mobiles/Boss.java
@@ -1,14 +1,21 @@
 package universite_paris8.iut.bak.timetowerdefense.modele.entites.mobiles;
 
 import javafx.geometry.Point2D;
+import universite_paris8.iut.bak.timetowerdefense.modele.entites.statiques.Tour;
 
 import java.util.List;
 
-public class Boss extends Ennemi{
+public abstract class Boss extends Ennemi{
 
+    private double tempsRecharge;
 
-    public Boss(double x, double y, int pv, int vitesseBase, int recompense, List<Point2D> chemin) {
+    public Boss(double x, double y, int pv, int vitesseBase, int recompense, int tempsRecharge, List<Point2D> chemin) {
         super(x, y, pv, vitesseBase, recompense, chemin);
+        this.tempsRecharge = tempsRecharge;
     }
+
+    public abstract void competence(List<Tour> tours);
+
+
 
 }
